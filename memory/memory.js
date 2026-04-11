9990098dd6be0aae2cd89c52dd3800bbc72e2ff6
RFMfly <rfmaude.autre@gmail.com>
2026-04-11 17:52:51 +0200

    memory

--- memory/memory.js
+++ memory/memory.js
@@ -2,52 +2,54 @@ const foodTheme = ['🍎', '🍌', '🍇', '🥦', '🍕', '🍦', '🍩', '🥑
 const animalsTheme = ['🦁', '🐸', '🦒', '🐧', '🐙', '🦋', '🦉', '🐘', '🐷', '🐝'];
 const adventureTheme = ['🚀', '🚲', '🚁', '⛵', '🚜', '🛸', '🚂', '🚒', '🛹', '🎈'];
 const objectsTheme = ['🎁', '🕶️', '🎸', '☂️', '🔑', '🎨', '⚽', '⏰', '💡', '🌵'];
-const theme = [foodTheme, adventureTheme, animalsTheme, objectsTheme]
+const objects2Theme = ['🎁', '🕶️'];
+const theme = [foodTheme, adventureTheme, animalsTheme, objectsTheme, objects2Theme]
 
 const gameState = {
-  premiereCarte: null,
-  deuxiemeCarte: null,
-  stopclick: false,
-  nbCoups: 0,
-  secondes: 0,
-  interval : null,
-  etatChrono: false
-
-
-
+    premiereCarte: null,
+    deuxiemeCarte: null,
+    stopclick: false,
+    nbCoups: 0,
+    secondes: 0,
+    interval: null,
+    etatChrono: false
 };
 
 const zoneJeu = document.querySelector(".jeu");
-const btnReinitialiser = document.querySelector(".reinitialiser");
+const start = document.querySelector(".checkstart");
+const btnReinitialiser = document.querySelectorAll(".reinitialiser");
 const themeSelect = document.querySelector("#topic-select");
 const divTheme = document.querySelector(".div-theme");
-const start = document.querySelector(".checkstart");
 const defi = document.querySelector(".defi");
 const coup = document.querySelector(".coups");
 const tempsAffiche = document.querySelector('.temps');
+const confettis = document.querySelector('#confetti-canvas');
+/*modale*/
+const overlay = document.querySelector(".overlay");
+const fermerModaleBtn = document.querySelector(".fermermodale");
 
 
 
 /*ZONE SCORE + TEMPS */
 function updateScore() {
-  coup.textContent = `Nombre de coups : ${gameState.nbCoups}`;
+    coup.textContent = `Nombre de coups : ${gameState.nbCoups}`;
 }
 function formatTime(seconds) {
 
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
 function updateTime() {
-    const resTime =formatTime(gameState.secondes)
+    const resTime = formatTime(gameState.secondes)
 
-  tempsAffiche.textContent = `Temps : ${resTime}` ;
+    tempsAffiche.textContent = `Temps : ${resTime}`;
 }
 
 
 function demarrerChrono() {
-    if (gameState.etatChrono===false){
-        gameState.etatChrono=true
+    if (gameState.etatChrono === false) {
+        gameState.etatChrono = true
         gameState.interval = setInterval(() => {
             gameState.secondes++
             updateTime()
@@ -55,9 +57,9 @@ function demarrerChrono() {
     }
 
 }
-function arreterChrono(){
+function arreterChrono() {
     clearInterval(gameState.interval);
-    gameState.etatChrono= false;
+    gameState.etatChrono = false;
     gameState.interval = null;
 
 }
@@ -74,7 +76,7 @@ function choisirTheme() {
     if (!theme[indexTheme]) return null;
 
     return theme[indexTheme];
-    
+
 }
 
 
@@ -85,15 +87,14 @@ function preparation() {
 
     gameState.nbCoups = 0;
     updateScore();
-    
 
     const deck = [...choix, ...choix]
     deck.sort(() => Math.random() - 0.5);
     generate(deck)
     divTheme.style.display = "none";
     defi.style.display = "grid";
+    console.log(deck);
 }
-start.addEventListener("click", preparation);
 
 
 function generate(tab) {
@@ -109,7 +110,7 @@ function generate(tab) {
 
 }
 function retourner() {
-        demarrerChrono()
+    demarrerChrono()
     // SÉCURITÉ : On ne fait rien si le jeu est bloqué OU si on clique sur une carte déjà retournée
     if (gameState.stopclick || this.classList.contains("retournee")) return;
 
@@ -122,33 +123,84 @@ function retourner() {
         // C'est le deuxième clic
         gameState.deuxiemeCarte = this;
         gameState.stopclick = true;
-        gameState.nbCoups ++;
+        gameState.nbCoups++;
         updateScore();
         verifier(); // On lance la comparaison
     }
 }
 
 function verifier() {
+
     if (gameState.premiereCarte.dataset.emoji === gameState.deuxiemeCarte.dataset.emoji) {
         gameState.premiereCarte.classList.add("match");
         gameState.deuxiemeCarte.classList.add("match");
         gameState.premiereCarte = null;
         gameState.deuxiemeCarte = null;
         gameState.stopclick = false;
+        win();
+
+
     } else {
         setTimeout(() => {
             gameState.premiereCarte.classList.remove("retournee");
             gameState.deuxiemeCarte.classList.remove("retournee");
 
             gameState.premiereCarte = null;
-            gameState.deuxiemeCarte = null;
             gameState.stopclick = false;
         }, 2000);
     }
 }
 
 themeSelect.selectedIndex = 0;
-preparation();
+
+
+function win() {
+    const themeActuel = choisirTheme();
+    if (!themeActuel) return;
+
+    const cartesMatch = document.querySelectorAll(".carte.match");
+    if (cartesMatch.length === themeActuel.length * 2) {
+        arreterChrono();
+        overlay.classList.add("active");
+
+        var count = 200;
+        var defaults = {
+            origin: { y: 0.7 }
+        };
+
+        function fire(particleRatio, opts) {
+            confetti({
+                ...defaults,
+                ...opts,
+                particleCount: Math.floor(count * particleRatio)
+            });
+        }
+
+        fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+        fire(0.2, {
+            spread: 60,
+        });
+        fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+    }
+}
+
 
 function reinitialiser() {
     gameState.premiereCarte = null;
@@ -164,9 +216,17 @@ function reinitialiser() {
     updateScore();
     updateTime();
     preparation();
-
-        
-
-
+    fermerModale();
 }
-btnReinitialiser.addEventListener("click", reinitialiser);
+function fermerModale() {
+    overlay.classList.remove("active");
+}
+
+/*ÉVÉNEMENTS*/
+start.addEventListener("click", preparation);
+preparation();
+fermerModaleBtn.addEventListener("click", fermerModale);
+
+btnReinitialiser.forEach((btn) => { /*Pour que les deux boutons de réinitialisation fonctionnent*/
+    btn.addEventListener("click", reinitialiser);
+});
